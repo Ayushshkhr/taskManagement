79850074d63176d0da58c810e895f91ad4355845
Ayush Shekhar <ayushshkhr01.com>
2026-05-28 16:18:28 +0530

    Add environment examples and API config

--- FE/t-m-a/src/api/axios.js
+++ FE/t-m-a/src/api/axios.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
 });
 
 API.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
     const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
